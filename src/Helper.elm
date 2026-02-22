f37b5a45ac7bc78cc1cbef1f945bacc5b0a149f1
Marco <up230743@alumnos.upa.edu.mx>
2026-02-22 16:45:29 -0600

    Primera prueba de funcionamiento

--- src/Helper.elm
+++ src/Helper.elm
@@ -9,3 +9,46 @@ import Html.Attributes
 --main : Html.Html msg
 --main =
 --    Html.div [] [ headers "Titulos", hyperlink "https://upa.edu.mx" "My School" ]
+--Ejercicio 1 (joinWords)
+
+
+joinWords : String -> String -> String
+joinWords word1 word2 =
+    word1 ++ word2
+
+
+
+--Ejercicio 2 (upperChar)
+
+
+isUpperChars : List Char -> List Bool
+isUpperChars list =
+    List.map Char.isUpper list
+
+
+
+--Ejercicio 3 (evalChar)
+
+
+evalChars : List Char -> (Char -> Bool) -> List Bool
+evalChars list funTrans =
+    List.map funTrans list
+
+
+
+--EJERCICIOS DE HTML
+--Ejercicio 1 (Headers)
+
+
+headers : String -> Html.Html msg
+headers param =
+    Html.div [] [ Html.h1 [] [ Html.text param ], Html.h2 [] [ Html.text param ], Html.h3 [] [ Html.text param ], Html.h4 [] [ Html.text param ], Html.h5 [] [ Html.text param ], Html.h6 [] [ Html.text param ] ]
+
+
+
+--Ejercicio 2 (hyperlink)
+
+
+hyperlink : String -> String -> Html.Html msg
+hyperlink url textoVisible =
+    Html.a [ Html.Attributes.href url ] [ Html.text textoVisible ]
